c64a2e58780cdd34d7edddfe31f7e7c0b7810e5c
winterilee <plumtree9612@gmail.com>
2023-11-16 23:26:58 -0800

    continue brushing up my python knowledge by revising notes

---
+++ pythonFundamentals1.1.py
@@ -0,0 +1,75 @@
+# Numbers
+# *int -> whole numbers, positive or negative.
+# *float -> decimal numbers, positive or negative.
+# *complex -> are a part of the real number system and are often referenced with the letter "j".
+
+# Type
+print(type(99))
+print(type(7.72))
+print(type(2j))
+
+# Conversion
+int_to_float = float(22)
+float_to_int = int(9.18)
+int_to_complex = complex(88)
+
+# Random Number
+# Python doesn't have a built in random, so it needs to be imported.
+import random
+# Provide a random number between 1 and 9:
+print(random.randint(1,9))
+
+
+# Strings
+# Strings are any sequence of characters enclosed in single or double quotes.
+print("This is a string")
+
+# Concatenating Strings and Variables with the print function
+breakfast = "Blueberry muffins"
+print("Today's breakfast is ", breakfast)
+print("Today's breakfast is " + breakfast)
+
+# Type Casting or Explicit Type Conversion
+# *integer to string
+# The following will not work:
+# print("The temperature is " + 77)
+# The following will work:
+print("The temperature is " + str(77))
+
+# *string to integer
+value1 = 22
+value2 = "44"
+# The following will not work:
+# total1 = value1 + value2
+# The following will work:
+total2 = value1 + int(value2)
+
+# String Interpolation
+# We can inject variables into our strings, which is known as "string interpolation".
+menu = "pumpkin soup"
+amount1 = 1
+amount2 = 2
+measurement1 = "tsp"
+measurement2 = "tbsp"
+# *F-String
+print(f"The {menu} needs {amount1} {measurement1} of salt and {amount2} {measurement2} of sugar.")
+
+# *string.format()
+print("The {} needs {} {} of salt and {} {} of sugar.".format(menu, amount1, measurement1, amount2, measurement2))
+
+# *%-formatting
+# "%s" for a string and "%d" for a number.
+print("The %s needs %s %s of salt and %s %s of sugar." % (menu, amount1, measurement1, amount2, measurement2))
+
+# Built-In String Methods
+# *string.title(): returns a copy of the string with all the first character of words in uppercase.
+# *string.upper(): returns a copy of the string with all the characters in uppercase.
+# *string.lower(): returns a copy of the string with all the characters in lowercase.
+# *string.count(substring): returns number of occurrences of substring in string.
+# *string.split(char): returns a list of values where string is split at the given character. Without a parameter the default split is at every space.
+# *string.find(substring): returns the index of the start of the first occurrence of substring within string.
+# *string.isalnum(): returns boolean depending on whether the string's length is > 0 and all characters are alphanumeric (letters and numbers only). Strings that include spaces and punctuation will retun False for this method. Similar methods include ".isalpha()", ".isdigit()", ".islower()", ".isupper()", and so on. All return booleans.
+# *string.join(list): returns a string that is all strings within our set (in this case a list) concatenated.
+# *string.endswith(substring): returns a boolean based upon whether the last characters of string match substring.
+
+
--- pythonFundamentals1.py
+++ pythonFundamentals1.py
@@ -7,27 +7,22 @@
 
 # Primitive data types
 # *Numbers -> Integers(whole numbers), floating point numbers(decimal numbers), and complex numbers.
-
 age = 28
 weight = 130.28
 
 # *Strings -> Text.
-
 fruit = "Blueberry"
 
 # *Boolean Values -> Assesses the truth value of something. It has only two values: True and False(Uppercase T and F).
-
 is_hungry = False
 
 
 # Composite types
 # *Tuples -> A type of data that is "immutable" (can't be modified after its creation) and can hold a group of values. Tuples can contain mixed data types.
-
 cat = ("Skippy", "Calico", 3, False)
 print(cat[0])
 
 # *Lists -> A type of data that is "mutable" and can hold a group of values. Usually meant to store a collection of related data.
-
 vegetables = ["Celery", "Onion", "Cucumber"]
 vegetables[1] = "Asparagus"
 vegetables.append("Broccoli")
@@ -35,7 +30,6 @@ print(vegetables)
 vegetables.pop()
 
 # *Dictionaries -> A group of key-value pairs. Dictionary elements are indexed by unique keys which are used to access values.
-
 rescue_dog = {"name": "Beau", "age": 1, "weight": 9.2, "vaccinated": False}
 rescue_dog["vaccinated"] = True
 print(rescue_dog)
@@ -43,11 +37,9 @@ print(rescue_dog)
 
 # Common Functions
 # Use "type" function to find out the data type for variables:
-
 print(type(3.14))
 print(type(rescue_dog))
 
 # Use "len" function to get the length:
-
 print(len(rescue_dog))
 print(len("Tomato"))
