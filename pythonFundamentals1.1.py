cc0641c8e8f3aa564fcaa066a449b32a522443f9
winterilee <plumtree9612@gmail.com>
2023-11-17 21:27:39 -0800

    continue revising notes to study python

--- pythonFundamentals1.1.py
+++ pythonFundamentals1.1.py
@@ -71,5 +71,3 @@ print("The %s needs %s %s of salt and %s %s of sugar." % (menu, amount1, measure
 # *string.isalnum(): returns boolean depending on whether the string's length is > 0 and all characters are alphanumeric (letters and numbers only). Strings that include spaces and punctuation will retun False for this method. Similar methods include ".isalpha()", ".isdigit()", ".islower()", ".isupper()", and so on. All return booleans.
 # *string.join(list): returns a string that is all strings within our set (in this case a list) concatenated.
 # *string.endswith(substring): returns a boolean based upon whether the last characters of string match substring.
-
-
