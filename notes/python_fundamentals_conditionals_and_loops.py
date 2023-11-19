ec74b3a562b62a341e7a6ce40116905d8dec9e46
winterilee <plumtree9612@gmail.com>
2023-11-18 22:00:52 -0800

    continue revising and studying notes

---
+++ notes/python_fundamentals_conditionals_and_loops.py
@@ -0,0 +1,27 @@
+# Conditionals
+# Conditional statements allow us to run certain lines of code depending on whether certain conditions are met.
+# Python uses "if", "elif", and "else".
+grade = 82
+if grade >= 90:
+    print("A")
+elif grade >= 80:
+    print("B")
+elif grade >= 70:
+    print("C")
+elif grade >= 60:
+    print("D")
+else:
+    print("F")
+
+#Operators for conditionals:
+# * == : Checks if the value of two operands are equal.
+# * != : Checks if the value of two operands are not equal.
+# * >  : Checks if the value of left operand is greater than the value of right operand.
+# * <  : Checks if the value of left operand is less than the value of right operand.
+# * >= : Checks if the value of left operand is greater than or equal to the value of right operand.
+# * <= : Checks if the value of left operand is less than or equal to the value of right operand.
+# * and: Checks that each expression on the left and right are both True.
+# * or : Checks if either the expression on the left or right is True.
+# * not: Reverses the true-false value of the operand.
+
+# Loops
