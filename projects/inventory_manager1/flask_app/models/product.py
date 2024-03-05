418cdf1939b94e4ca10ea689b2361a55b8123873
winterilee <plumtree9612@gmail.com>
2024-03-04 16:13:38 -0800

    making a progress

--- projects/inventory_manager1/flask_app/models/product.py
+++ projects/inventory_manager1/flask_app/models/product.py
@@ -1,4 +1,5 @@
 from flask_app.config.mysqlconnection import connectToMySQL
+from flask import flash
 
 class Product:
     db = "python_inventory_manager1_schema"
@@ -14,3 +15,39 @@ class Product:
     def create_product(cls, data):
         query = "INSERT INTO products (product_name, product_count, inventory_id) VALUES (%(product_name)s, %(product_count)s, %(inventory_id)s);"
         return connectToMySQL(cls.db).query_db(query, data)
+    
+    @classmethod
+    def get_all(cls):
+        query = "SELECT * FROM products JOIN inventories ON products.inventory_id = inventories.id;"
+        results = connectToMySQL(cls.db).query_db(query)
+        product_list = []
+        for row in results:
+            one_product = {
+                "id": row["id"],
+                "product_name": row["product_name"],
+                "product_count": row["product_count"],
+                "inventory_id": row["inventories.id"]
+            }
+            product_list.append(one_product)
+        return product_list
+    
+    @classmethod
+    def get_one(cls, product_id):
+        query = "SELECT * FROM products WHERE id = %(id)s;"
+        data = {"id": product_id}
+        result = connectToMySQL(cls.db).query_db(query, data)
+        return cls(result[0])
+    
+    @staticmethod
+    def validate_product(product):
+        is_valid = True
+        if len(product["product_name"]) <= 0:
+            flash("Product name is required.", "product")
+            is_valid = False
+        elif len(product["product_name"]) < 3:
+            flash("Product name must be at least 3 characters.", "product")
+            is_valid = False
+        if product["product_count"] <= 0:
+            flash("Invalid amount.", "product")
+            is_valid = False
+        return is_valid
