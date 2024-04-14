b965e78f3c764bd4305edcafc151946d1faebe85
winterilee <plumtree9612@gmail.com>
2024-04-13 22:55:39 -0700

    debugging lines

--- projects/inventory_manager1/flask_app/controllers/products.py
+++ projects/inventory_manager1/flask_app/controllers/products.py
@@ -6,7 +6,10 @@ from flask_app.models import product
 def new_product(inventory_id):
     if "logged_in_id" not in session:
         return redirect(url_for("index"))
-    return render_template("new_product.html", inventory_id)
+    if "current_inventory" in session:
+        session.pop("current_inventory")
+    session["current_inventory"] = inventory_id
+    return render_template("new_product.html")
 
 @app.route("/create_product", methods = ["POST"])
 def create_product():
@@ -18,7 +21,7 @@ def create_product():
         data = {
             "product_name": request.form["product_name"],
             "product_count": int(request.form["product_count"]),
-            "inventory_id": session["current_inventory"]
+            "inventory_id": request.form["inventory_id"]
         }
         product.Product.create_product(data)
     return redirect("/view_inventory/<int:session['current_inventory']>")
